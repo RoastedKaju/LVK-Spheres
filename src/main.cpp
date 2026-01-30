3d122e86633d3f59a90684db96b86344feda95df
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2026-01-30 20:40:27 +0500

    Add sample code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <memory>
+#include <vector>
+
+#include <lvk/LVK.h>
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	minilog::LogConfig configInfo{};
+	configInfo.threadNames = false;
+	minilog::initialize(nullptr, configInfo);
+
+	int width = -65;
+	int height = -90;
+
+	GLFWwindow* window = lvk::initWindow("Example", width, height);
+
+	// Context
+	std::unique_ptr<lvk::IContext> ctx = lvk::createVulkanContextWithSwapchain(window, width, height, {});
+
+	// Shaders
+
+	// Pipelines
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwGetFramebufferSize(window, &width, &height);
+
+		if (!width || !height)
+			continue;
+
+		// Command buffer
+	}
+
+	ctx.reset();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return EXIT_SUCCESS;
 }
